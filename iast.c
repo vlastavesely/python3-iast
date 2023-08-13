0922e24e3873b1f42c2f6cda775017f34d115474
Vlasta Vesely <vlastavesely@protonmail.ch>
2023-08-13 14:05:34 +0200

    implement transliteration

--- iast.c
+++ iast.c
@@ -1,13 +1,44 @@
 #include <Python.h>
+#include <iast/transliteration.h>
 
 static PyObject *iast(PyObject* self, PyObject* args)
 {
-	Py_RETURN_NONE;
+	const char *in;
+	char *translit;
+	PyObject *ret;
+
+	if (PyArg_ParseTuple(args, "s", &in) == 0) {
+		return NULL;
+	}
+
+	if (transliterate_devanagari_to_latin(in, &translit) != 0) {
+		return NULL;
+	}
+
+	ret = PyUnicode_FromString(translit);
+	free(translit);
+
+	return ret;
 }
 
 static PyObject *deva(PyObject* self, PyObject* args)
 {
-	Py_RETURN_NONE;
+	const char *in;
+	char *translit;
+	PyObject *ret;
+
+	if (PyArg_ParseTuple(args, "s", &in) == 0) {
+		return NULL;
+	}
+
+	if (transliterate_latin_to_devanagari(in, &translit) != 0) {
+		return NULL;
+	}
+
+	ret = PyUnicode_FromString(translit);
+	free(translit);
+
+	return ret;
 }
 
 static PyMethodDef module_methods[] =
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@
 
 from distutils.core import setup, Extension
 
-iast = Extension('iast', sources=['iast.c'])
+iast = Extension('iast', sources=['iast.c'], libraries=['iast'])
 
 setup(name='IAST', version='1.0', ext_modules=[iast])
